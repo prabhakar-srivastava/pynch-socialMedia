0f83e950c84ac0f6e010c94acb1d492a246b15c7
prabhakar <prabhakarsrivastava73@gmial.com>
2022-07-05 18:25:16 +0530

    comp

--- functions/functions.php
+++ functions/functions.php
@@ -3,7 +3,7 @@
 function connect() {
     static $conn;
     if ($conn === NULL){ 
-        $conn = mysqli_connect('localhost','root','','socialnetwork');
+        $conn = mysqli_connect('sql210.epizy.com','epiz_32106415','krEz3LYubEL5xki','epiz_32106415_socialnetwork');
     }
     return $conn;
 }
